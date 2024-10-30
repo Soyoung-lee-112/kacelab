d5bb1809e93626070a918bd653bf0b05476f5a24
Soyoung-lee-112 <s.young1922@gmail.com>
2024-10-30 15:00:18 +0900

    header 콘텐츠 추가

--- js/common.js
+++ js/common.js
@@ -7,7 +7,24 @@
     //     smooth: true
     // });
 
+const logoImages = [document.getElementById('logoImg1'), document.getElementById('logoImg2')];
 
+document.querySelector('.h__logo').addEventListener('mouseover', () => {
+  logoImages.forEach(img => { //배열의 요소 순서대로 값을 갖는다. 
+    img.dataset.originalSrc = img.src;
+    img.src = 'images/logo2.png';
+  });
+});
+
+document.querySelector('.h__logo').addEventListener('mouseout', () => {
+  logoImages.forEach(img => {
+    img.src = img.dataset.originalSrc;
+  });
+});
 
 
 })();
+
+
+
+
